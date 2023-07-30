b6d7259ede4052488aea88dd105d735c636d530c
WatabeAYumi1018 <134474084+WatabeAYumi1018@users.noreply.github.com>
2023-07-30 12:24:51 +0900

    tnl::meshとtnl::cameraの使用方法確認中

--- ManagedDxlGame/program/engine/backGround.cpp
+++ ManagedDxlGame/program/engine/backGround.cpp
@@ -8,23 +8,37 @@ backGround::backGround() {
 backGround::~backGround() { Finalize(); }
 
 void backGround::Initialize() {
-	m_mesh = Mesh::CreatePlane({ 10,10,0 });
+
+	//ƒJƒƒ‰Ý’è
+	m_camera = new Camera(DXE_WINDOW_WIDTH, DXE_WINDOW_HEIGHT);
+	m_camera->pos_ = { 100,100,0 };
+//	m_camera->target_ = { 0,0,0 };
+
+	//ƒƒbƒVƒ…‚É”wŒi‰æ‘œ‚ðÝ’è
+	m_mesh = Mesh::CreatePlaneMV({1000,1000,0});
 	m_mesh->setTexture(Texture::CreateFromFile("graphics/PT_Skybox_Texture_01.png"));
+	m_mesh->pos_= { -1000,0,500 };
 }
 
 void backGround::Update(float delta_time) {
-	Draw();
+	m_mesh->render(m_camera);
+	m_camera->update();
+//	Draw();
 }
-
-void backGround::Draw() {
-	if(m_mesh) {
-		DrawGraph(m_pos.x,m_pos.y, back_hdl,true);
-	}
-}
+//
+//void backGround::Draw() {
+//	if(m_mesh) {
+//		DrawGraph(m_pos.x,m_pos.y, back_hdl,true);
+//	}
+//}
 
 void backGround::Finalize() {
 	if (m_mesh) {
-		delete this;
+		delete m_mesh;
 		m_mesh = nullptr;
+	}
+	if (m_camera) {
+		delete m_camera;
+		m_camera = nullptr;
 	}
 }
--- ManagedDxlGame/program/engine/backGround.h
+++ ManagedDxlGame/program/engine/backGround.h
@@ -13,11 +13,13 @@ private:
 	int back_hdl=0;				//背景画像ハンドル
 	Vector3 m_pos = {0,0,500};	//座標（後々多態性で削除。とりあえず）
 	Mesh* m_mesh = nullptr;		//メッシュ
+	Camera* m_camera = nullptr;
+
 
 public:
 	//-----メンバ関数-----//
 	void Initialize();
 	void Update(float delta_time);
-	void Draw();
+	//void Draw();
 	void Finalize();
 };
